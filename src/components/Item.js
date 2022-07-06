a701fba97cfc5f5d9de78ca58e63afc6ca72ba06
amalia <amalia.reodica@gmail.com>
2022-07-06 17:31:19 -0400

    update app for event handlers

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,172 +1,65 @@
 import '../App.css';
 import React from 'react'
+import products from '../data/product.js'
+import {Context} from '../Context'
 
-const cake = [
-  {
-    'CakeName': 'Round Cake',
-    'BasePrice': 45,
-    'Size': [
-      ['6 inch, two layer cake', 0],
-      ['6 inch, three layer cake',15],
-      ['8 inch, two layer cake',30]
-    ],
-    'CakeBatterFlavour': [
-      'Chocolate', 
-      'Vanilla', 
-      'Birthday Cake', 
-      'Red Velvet', 
-      'Ube', 
-      'Mocha', 
-      'Strawberry Shortcake', 
-      'Fruit cake'
-    ],
-    'ButtercreamIcingFlavour': [
-      'Chocolate', 
-      'Vanilla', 
-      'Peanut Butter', 
-      'Cream Cheese', 
-      'Oreo', 
-      'Chocolate Hazelnut (Nutella)', 
-      'Ube', 
-      'Mocha'
-    ],
-    'Addons': [
-      ['Cheesecake layer', 5],
-      ['Peanut Butter Cheesecake layer', 5],
-      ['Chocolate Mousse', 4],
-      ['White Chocolate Mousse', 4],
-      ['Chocolate Fudge', 4],
-      ['Crushed Almonds', 2],
-      ['Raspberry Puree', 2],
-      ['Custom Cake Topper', 8],
-    ]
-  },
-  {
-    'CakeName': 'Square Cake',
-    'BasePrice': 45,
-    'Size': [
-      ['6 inch, two layer cake', 0],
-      ['6 inch, three layer cake',15],
-      ['8 inch, two layer cake',30]
-    ],
-    'CakeBatterFlavour': [
-      'Chocolate', 
-      'Vanilla', 
-      'Birthday Cake', 
-      'Red Velvet', 
-      'Ube', 
-      'Mocha', 
-      'Strawberry Shortcake', 
-      'Fruit cake'
-    ],
-    'ButtercreamIcingFlavour': [
-      'Chocolate', 
-      'Vanilla', 
-      'Peanut Butter', 
-      'Cream Cheese', 
-      'Oreo', 
-      'Chocolate Hazelnut (Nutella)', 
-      'Ube', 
-      'Mocha'
-    ],
-    'Addons': [
-      ['Cheesecake layer', 5],
-      ['Peanut Butter Cheesecake layer', 5],
-      ['Chocolate Mousse', 4],
-      ['White Chocolate Mousse', 4],
-      ['Chocolate Fudge', 4],
-      ['Crushed Almonds', 2],
-      ['Raspberry Puree', 2],
-      ['Custom Cake Topper', 8],
-    ]
-  }
+export default function Item() {
+  const { 
+    itemInfo,
+    quotePrice,
+    setItemInfo,
+    setQuotePrice,
+    updateAmount,
+    selectOne,
+    selectMultiple 
+  } = React.useContext(Context)
+  const cake = products
+  const [selected, setSelected] = React.useState(false)
+  
+  const handleItemClick = (index, cakeItem) => {
+    console.log('cakeeey', cakeItem)
+    setSelected(index)
+    itemInfo['Cake Name'] = cakeItem['Cake Name']
+    itemInfo['Base Price'] = cakeItem['Base Price']
+    setItemInfo(itemInfo)
+    setQuotePrice(cakeItem['Base Price'])
+    console.log(itemInfo)
+  }  
     
-]
-
-const updateAmount = (amount) => {
-  return amount !== 0 && amount.toLocaleString('en-US', {style: 'currency', currency: 'CAD'})
-}
-
-const selectOne = (title, input) => {
-  console.log(input)
-  const result = input.map((i, index) => {
-    if (typeof(i[1]) === 'number') {
-      const [name, amount] = i
-      return (
-        <div className='option' id={`option-${name}`}>
-          <label htmlFor={`option-${name}`}>
-            <p className="option-title">{name}</p>
-          </label>
-          <input className="option-input" type='radio' id={`option-${name}`} /> 
-          <p className="option-pricing">{updateAmount(amount)}</p>
-        </div>
-      )
-    } else {
-      return (
-        <div className='option' id={`option-${i}`}>
-          <label htmlFor={`option-${i}`}>
-            <p className="option-title">{i}</p>
-          </label>
-          <input className="option-input" type='radio' id={`option-${i}`} /> 
-        </div>
-      )
-    }
-    
-  })
-  return (
-    <div id={title}>
-      <h5>{title}</h5>
-      {result}
-    </div>
-  )
-}
-
-const selectMultiple = (title, input) => {
-  console.log(input)
-  const result = input.map((i, index) => {
-    const [name, amount] = i
+  const itemElements = cake.map((i, index) => {
+    console.log(i, index)
+    const showForm = selected === index
     return (
-      <div className='option' id={`option-${i}`}>
+      <div className={`${showForm ? 'item' :'button-item'}`}>
+        <h4 onClick={() => handleItemClick(index, i)}>{i['Cake Name']}</h4>
+        <p className={`base-price ${showForm ? '' : 'invisible'}`}>Base Price: {updateAmount(i['Base Price'])}</p>
+        {showForm && 
+        selectOne('Size', i['Size'])}
+        {showForm && selectOne('Cake Batter Flavour', i['Cake Batter Flavour'])}
+        {showForm && selectOne('Buttercream Icing Flavour', i['Buttercream Icing Flavour'])}
+        {showForm && selectMultiple('Add-ons', i['Add ons'])}
+      
         
-        <label htmlFor={`option-${name}`}>
-            <p className="option-title">{name}</p>
-        </label>
-        <input className="option-input" type='checkbox' id={`option-${name}`} />
-        <p className="option-pricing">{updateAmount(amount)}</p>
       </div>
     )
   })
-  return (
-    <div id={title}>
-      <h5>{title}</h5>
-      {result}
-    </div>
-  )
-}
 
-export default function Item() {
-    const [input, setInput] = React.useState()
-    
-    const itemElements = cake.map(i => {
-      return (
-        <div className='item'>
-          <h4>{i.CakeName}</h4>
-          <p className="base-price">Base Price: {updateAmount(i.BasePrice)}</p>
-          {selectOne('Size', i.Size)}
-          {selectOne('Cake Batter Flavour', i.CakeBatterFlavour)}
-          {selectOne('Buttercream Icing Flavour', i.ButtercreamIcingFlavour)}
-          {selectMultiple('Add-ons', i.Addons)}
+  const tmpfunction = (obj) => {
+    const tmp = Object.entries(obj).map(ii => (<li>{ii[0]} {ii[1]}</li>))
+    return (
+      <ul>
+        {tmp}
+      </ul>
+    )
+  }
 
-          
-        </div>
-      )
-    })
   return (
     <div>
-        
-        
+        <h3>Products</h3>
+
+        {quotePrice}
+        {tmpfunction(itemInfo)}
         <section className="menu-item">
-            <h3>Cupcakes</h3>
             {itemElements}
         </section>
     </div>
---
+++ src/data/product.js
@@ -0,0 +1,83 @@
+const cake = [
+    {
+      'Cake Name': 'Round Cake',
+      'Base Price': 45,
+      'Size': [
+        ['6 inch, two layer cake', 0],
+        ['6 inch, three layer cake',15],
+        ['8 inch, two layer cake',30]
+      ],
+      'Cake Batter Flavour': [
+        'Chocolate', 
+        'Vanilla', 
+        'Birthday Cake', 
+        'Red Velvet', 
+        'Ube', 
+        'Mocha', 
+        'Strawberry Shortcake', 
+        'Fruit cake'
+      ],
+      'Buttercream Icing Flavour': [
+        'Chocolate', 
+        'Vanilla', 
+        'Peanut Butter', 
+        'Cream Cheese', 
+        'Oreo', 
+        'Chocolate Hazelnut (Nutella)', 
+        'Ube', 
+        'Mocha'
+      ],
+      'Add ons': [
+        ['Cheesecake layer', 5],
+        ['Peanut Butter Cheesecake layer', 5],
+        ['Chocolate Mousse', 4],
+        ['White Chocolate Mousse', 4],
+        ['Chocolate Fudge', 4],
+        ['Crushed Almonds', 2],
+        ['Raspberry Puree', 2],
+        ['Custom Cake Topper', 8],
+      ]
+    },
+    {
+      'Cake Name': 'Square Cake',
+      'Base Price': 45,
+      'Size': [
+        ['6 inch, two layer cake', 0],
+        ['6 inch, three layer cake',15],
+        ['8 inch, two layer cake',30]
+      ],
+      'Cake Batter Flavour': [
+        'Chocolate', 
+        'Vanilla', 
+        'Birthday Cake', 
+        'Red Velvet', 
+        'Ube', 
+        'Mocha', 
+        'Strawberry Shortcake', 
+        'Fruit cake'
+      ],
+      'Buttercream Icing Flavour': [
+        'Chocolate', 
+        'Vanilla', 
+        'Peanut Butter', 
+        'Cream Cheese', 
+        'Oreo', 
+        'Chocolate Hazelnut (Nutella)', 
+        'Ube', 
+        'Mocha'
+      ],
+      'Add ons': [
+        ['Cheesecake layer', 5],
+        ['Peanut Butter Cheesecake layer', 5],
+        ['Chocolate Mousse', 4],
+        ['White Chocolate Mousse', 4],
+        ['Chocolate Fudge', 4],
+        ['Crushed Almonds', 2],
+        ['Raspberry Puree', 2],
+        ['Custom Cake Topper', 8],
+      ]
+    }
+      
+  ]
+
+export default cake 
